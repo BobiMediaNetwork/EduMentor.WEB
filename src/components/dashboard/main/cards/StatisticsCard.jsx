7b36063476b79763a3887d479bd5d16dc26a0c85
Bolba-Mateescu Andrei <andrei_bolba@yahoo.com>
2025-12-20 00:03:23 +0200

    EDU-16 - add: logo in the app

--- src/components/dashboard/main/cards/StatisticsCard.jsx
+++ src/components/dashboard/main/cards/StatisticsCard.jsx
@@ -5,7 +5,7 @@ export function StatisticsCard({mainText, number, icon, bgColor}){
         <div className="bg-stone-100 h-full w-1/4 rounded-2xl shadow-xl flex">
             <div className="h-full w-4/6 flex flex-col justify-center pl-6">
                 <p className="text-sm text-stone-500">{mainText}</p>
-                <h1 className="font-bold text-3xl">{number}</h1>
+                <h1 className="font-bold text-3xl">{new Intl.NumberFormat('ro-RO').format(number)}</h1>
             </div>
             <div className="h-full w-2/6 flex items-center justify-center">
                 <div className={"h-16 w-16 bg-blue-100 rounded-2xl flex items-center justify-center "+bgColor}>
--- src/components/dashboard/sidebar/DashboardTitle.jsx
+++ src/components/dashboard/sidebar/DashboardTitle.jsx
@@ -1,6 +1,9 @@
+import logoIcon from '../../../assets/logo/icon.png'
+
 export function DashboardTitle() {
     return (
-        <div className="h-1/10 w-full flex pl-5 items-center text-2xl font-bold border-b border-slate-700 text-slate-200">
+        <div className="h-1/10 w-full flex pl-5 items-center text-2xl font-bold border-b border-slate-700 text-slate-200 gap-1">
+            <img src={logoIcon} alt="Logo" className="h-5 w-5 rounded-md" />
             EduMentor
         </div>
     )
